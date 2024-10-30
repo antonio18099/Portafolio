39abdf8581e7354e039b4d0af5d4ac885c0adb49
antonio18099 <antoniosanchezr0921@gmial.com>
2024-10-30 02:33:23 -0500

    commit "final"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,10 @@ const Footer = () => {
         <footer className={styles.footer}>
             <p className={styles.footerText}>Sígueme en mis redes sociales</p>
             <ul className={styles.socialLinks}>
-                <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
-                <li><a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                <li><a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                <li><a href="https://web.facebook.com/profile.php?id=100011498852293" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+                <li><a href="https://www.instagram.com/antonioszx_/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                <li><a href="https://github.com/antonio18099" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/antonio-sanchez-5a9758335/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
             </ul>
             <p className={styles.signature}>"Viviendo en modo avion"</p>
             <p className={styles.signature}>Creado con dedicación por Antonio José Rodríguez Sánchez</p>
